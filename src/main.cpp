905c7ffb7dd4cade4f0830af3f40a6eb538da3ea
Markus Werle <daixtrose@users.noreply.github.com>
2017-08-24 09:59:20 +0200

    reworking CMake system to directly download Google test

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include "arithmetics.hpp"
+#include <iostream>
 
 int main()
 {
-	std::cout << "TravisCI and Google Test in one project." << std:.endl;
+	std::cout << "TravisCI and Google Test in one project." << std::endl;
 	std::cout << "arithmetics::add(1, 1) = " << arithmetics::add(1, 1) << std::endl;
 	std::cout << "arithmetics::add_buggy(1, 1) = " << arithmetics::add_buggy(1, 1) << std::endl;
 }
