7f84c2c4f7243380fb1ceb133cdd3b39266fc62b
Markus Werle <daixtrose@users.noreply.github.com>
2017-08-24 14:06:22 +0200

    Split tests into 2 distict tests

--- test/some_unit_test.cpp
+++ test/some_unit_test.cpp
@@ -1,8 +1,11 @@
 #include "arithmetics.hpp"
 #include "gtest/gtest.h"
 
-TEST(AddTest, PerformsAdditionOnTwoIntegers) {
+TEST(TestAdd, PerformsAdditionOnTwoIntegers) {
   EXPECT_EQ(2, arithmetics::add(1, 1));
+}
+
+TEST(TestAdd, PerformsAdditionOnTwoIntegers_2) {
   EXPECT_EQ(2, arithmetics::add_buggy(1, 1));
 }
 
